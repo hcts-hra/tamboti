4762e54ef77ca459838f85e3a384555207427067
claudius108 <claudius.teodorescu@gmail.com>
2015-05-20 10:56:54 +0200

    Improved tamboti.createGuid().

--- resources/scripts/query.js
+++ resources/scripts/query.js
@@ -3,7 +3,7 @@ tamboti = {};
 tamboti.selectedSearchResultOptions = {};
 
 tamboti.createGuid = function() {
-	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	return 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 		var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
 		return v.toString(16);
 	});
