498115db9250f2d7cabec2cfd3fb9137df63b125
claudius108 <claudius.teodorescu@gmail.com>
2015-10-14 12:50:11 +0200

    Added support for changing the style of the selected tab.
    References #1493.

--- modules/edit/editor.js
+++ modules/edit/editor.js
@@ -1,10 +1,12 @@
 $(function() {
 
 	// this function has to be removed when the tabs will be completely refactored
-    function activateBotttomTab(tabIndex) {
-        $("table.bottom-tabs tr:first-child" td).each(function() {
-            $(this).css("background", "#EDEDED");
-        });
+    window.activateBotttomTab = function(tabIndex) {
+		$("table.bottom-tabs tr:first-child td").each(function() {
+		    $(this).css("background", "#ededed").css("border-bottom-color", "#bebebe");
+		});
+		$("table.bottom-tabs tr:first-child td:eq(" + tabIndex + ")").css("background", "white").
+		css("border-bottom-color", "white").css("color", "#3681B3");
     }
    
 });
